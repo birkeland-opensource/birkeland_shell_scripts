00607b427bc7ad6bc0d4d37dc6002beeaf3c6731
rubansundararaj <rubansundararaj@gmail.com>
2023-02-08 22:51:58 +0800

    updates

--- birkeland_installation.sh
+++ birkeland_installation.sh
@@ -145,6 +145,93 @@ make install
 
 echo "ending lnd installation"
 
+echo "Start Creating Bitcoin config"
+# Check if the .bitcoin folder exists, create it if not
+if [ ! -d "$HOME/.bitcoin" ]; then
+  mkdir "$HOME/.bitcoin"
+fi
+
+# Go to the .bitcoin folder
+cd "$HOME/.bitcoin"
+
+# Create the bitcoin.conf file if it doesn't exist, or append to it if it does
+{
+  echo "assumevalid=0"
+  echo "txindex=1"
+  echo "debug=net"
+  echo "# Allows LND to connect to the Bitcoin node via an RPC user & pass"
+  echo "rpcuser=birkeland"
+  echo "rpcpassword=birkeland"
+  echo "# Allows LND & Bitcoin Core to communicate via ZeroMQ"
+  echo "zmqpubrawblock=tcp://127.0.0.1:28332"
+  echo "zmqpubrawtx=tcp://127.0.0.1:28333"
+} >> bitcoin.conf
+
+echo "End Creating Bitcoin config"
+
+echo "Start creating LND config"
+# Check if the .lnd folder exists
+if [ ! -d "/root/.lnd" ]; then
+  # If it doesn't exist, create it
+  mkdir /root/.lnd
+fi
+
+# Create the lnd.conf file
+cat > /root/.lnd/lnd.conf << EOL
+## LND Settings
+# Lets LND know to run on top of Bitcoin (as opposed to Litecoin)
+bitcoin.active=true
+bitcoin.mainnet=true
+# Lets LND know you are running Bitcoin Core (not btcd or Neutrino)
+bitcoin.node=bitcoind
+## Bitcoind Settings
+# Tells LND what User/Pass to use to RPC to the Bitcoin node
+bitcoind.rpcuser=birkeland
+bitcoind.rpcpass=birkeland
+# Allows LND & Bitcoin Core to communicate via ZeroMQ
+bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332
+bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333
+## Zap Settings
+# Tells LND to listen on all of your computer's interfaces
+# This could alternatively be set to your router's subnet IP
+tlsextraip=0.0.0.0
+# Tells LND where to listen for RPC messages
+# This could also be set to your router's subnet IP
+rpclisten=0.0.0.0:10009
+#Specify the interfaces to listen on for p2p connections. One listen
+#address per line.
+# All ipv4 on port 9735:
+listen=0.0.0.0:9735
+EOL
+
+# Give the user confirmation that the script has finished running
+echo "The .lnd folder and lnd.conf file have been created."
+
+echo "End creating LND config"
+
+
+echo "Start installing mongodb"
+
+# Update the package manager
+sudo apt-get update
+
+# Install MongoDB
+sudo apt-get install -y mongodb
+
+# Create the data directory for MongoDB
+sudo mkdir -p /data/db
+
+# Change ownership of the data directory to the MongoDB user
+sudo chown -R mongodb:mongodb /data/db
+
+# Start MongoDB service
+sudo service mongodb start
+
+# Verify the MongoDB service status
+sudo service mongodb status
+
+
+echo "End Start installing mongodb"
 
 ufw allow 22
 ufw allow 9990/tcp
