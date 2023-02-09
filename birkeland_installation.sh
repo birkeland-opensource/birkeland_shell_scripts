2c9885e001998e2c7de47826025633bcd396102c
rubansundararaj <rubansundararaj@gmail.com>
2023-02-09 11:45:32 +0800

    add nginx port forwarding

--- birkeland_installation.sh
+++ birkeland_installation.sh
@@ -21,6 +21,28 @@ else
   exit 1
 fi
 
+# Create a new Nginx server block
+echo "start port forwarding 80 to 9990"
+cat > /etc/nginx/sites-available/default << EOL
+server {
+    listen 80;
+
+    location / {
+        proxy_pass http://localhost:9990;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOL
+
+# Restart Nginx to apply changes
+sudo service nginx restart
+
+echo "end of port forwarding 80 to 9990"
+
 echo "Installing nodejs..."
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 if [ $? -eq 0 ]
