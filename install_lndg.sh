798d84747fdc8688a51650179240b8132b2fe616
rubansundararaj <rubansundararaj@gmail.com>
2023-04-17 10:13:31 -0400

    add lndg installation

---
+++ install_lndg.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+# Clone the repository
+git clone https://github.com/cryptosharks131/lndg.git
+
+# Change directory into the repo
+cd lndg
+
+# Make sure you have python virtualenv installed
+sudo apt install -y virtualenv
+
+# Set up a python3 virtual environment
+virtualenv -p python3 .venv
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Install required dependencies
+pip install -r requirements.txt
+
+# Initialize some settings for your django site
+python initialize.py
+
+# The initial login user is lndg-admin and the password is output here:
+echo "The initial login user is lndg-admin."
+echo "The password can be found in the 'data/lndg-admin.txt' file."
+
+# Generate some initial data for your dashboard
+python jobs.py
+
+# Run the server via a python development server
+echo "Starting the development server on 0.0.0.0:8889"
+python manage.py runserver 0.0.0.0:8889
